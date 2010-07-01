13f2c91850f9412d3765baa5a61bd369656f42e3
Christopher.Bruns <Christopher.Bruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-01 21:29:33 +0000

    Use boost unit test framework for test. Begin creating python infrastructure. Point2D sort of works now.

--- cppsrc/include/geom2d.hpp
+++ cppsrc/include/geom2d.hpp
@@ -1,17 +1,28 @@
 #include <cassert>
+#include <boost/units/systems/si/length.hpp>
+#include <boost/units/units_fwd.hpp>
+#include <boost/units/base_dimension.hpp>
+#include <boost/units/base_unit.hpp>
+#include <boost/units/derived_dimension.hpp>
+#include <boost/units/make_system.hpp>
+#include <boost/units/io.hpp>
+#include <boost/units/quantity.hpp>
+#include <boost/units/static_constant.hpp>
+#include <boost/units/unit.hpp>
 
 namespace cmbcv {
 
     typedef double Real;
-    typedef Real Length; // TODO - use boost::units
-    typedef Real ScaleFactor; // TODO - use boost::units (dimensionless)
+    using boost::units::si::length;
+    using boost::units::quantity;
 
-    class Point2D {
+    class Point2D 
+    {
     private:
-        Length m_data[2]; // must be declared before x and y
+        quantity<length> m_data[2]; // must be declared before x and y
     public:
-        Length& x;
-        Length& y;
+        quantity<length>& x;
+        quantity<length>& y;
 
         Point2D() 
             : x(m_data[0]), y(m_data[1])
@@ -20,26 +31,30 @@ namespace cmbcv {
             m_data[1] = 0;
         }
 
-        Point2D(Length x, Length y) 
+        Point2D(const quantity<length>& x, const quantity<length>& y) 
             : x(m_data[0]), y(m_data[1])
         {
             m_data[0] = x;
             m_data[1] = y;
         }
 
-        Length operator[](int ix) {
+        Point2D(Real x, Real y, length u) 
+            : x(m_data[0]), y(m_data[1])
+        {
+            m_data[0] = x*u;
+            m_data[1] = y*u;
+        }
+
+        quantity<length>& operator[](int ix) {
             assert(ix >= 0);
             assert(ix <= 1);
             return m_data[ix];
         }
-    };
-
-    class AugmentedPoint2D : public Point2D {
-    };
-
-    class HomogeneousPoint2D : public Point2D {
-    private:
-        ScaleFactor w;
+        const quantity<length>& operator[](int ix) const {
+            assert(ix >= 0);
+            assert(ix <= 1);
+            return m_data[ix];
+        }
     };
 
 } // namespace cmbcv

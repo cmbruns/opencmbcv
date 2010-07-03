f7ba4f989a62dca20b4a6914c37712b3d8ba0087
Christopher.Bruns <Christopher.Bruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-03 16:22:41 +0000

    Started creating my own units system, since gccxml does not work with boost.units.

---
+++ cppsrc/include/measure.hpp
@@ -0,0 +1,47 @@
+
+// Pyplusplus/gccxml chokes on boost.units code
+// So create a crude facsimile here
+
+namespace cmbcv { namespace measure {
+
+// Physical dimension class
+template<
+    int LENEXP, 
+    int ANGLEEXP,
+    int PIXELEXP>
+struct physical_dimension_t {};
+
+typedef physical_dimension_t<1,0,0> length_t;
+typedef physical_dimension_t<2,0,0> Area;
+typedef physical_dimension_t<3,0,0> Volume;
+typedef physical_dimension_t<0,1,0> Angle;
+typedef physical_dimension_t<0,0,1> Pixels;
+
+// Unit class
+template<class PDIM>
+struct unit_t 
+{
+    typedef PDIM dimension_t;
+
+    // Adding or subtracting compatible units yield first unit
+    const unit_t<PDIM>& operator+(const unit_t<PDIM>& rhs) const {
+        return *this;
+    }
+    const unit_t<PDIM>& operator-(const unit_t<PDIM>& rhs) const {
+        return *this;
+    }
+};
+
+static unit_t<length_t> meter;
+
+// Quantity class
+template<class UNIT, class VALTYPE=double>
+struct quantity_t 
+{
+    typedef VALTYPE value_t;
+    typedef UNIT unit_t;
+
+    value_t value;
+};
+
+}} // namespace cmbcv

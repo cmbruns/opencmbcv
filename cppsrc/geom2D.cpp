cff3fc10fe057cffc185dd7653811b93804347c5
Christopher.Bruns <Christopher.Bruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-05 15:42:33 +0000

    Boost python code generation now runs

--- cppsrc/geom2D.cpp
+++ cppsrc/geom2D.cpp
@@ -2,14 +2,13 @@
 
 namespace cmbcv {
 
-Point2D Point2D::operator+(const Point2D& rhs) const {
-    const Point2D& lhs = *this;
-    return Point2D(lhs.x + rhs.x, lhs.y + rhs.y);
-}
-
-Point2D Point2D::operator-(const Point2D& rhs) const {
-    const Point2D& lhs = *this;
-    return Point2D(lhs.x - rhs.x, lhs.y - rhs.y);
-}
+    line2d_t homogeneous_point2d_t::line(const homogeneous_point2d_t& rhs) const {
+        // abuse vec3 cross product to get answer
+        const vec3_t& l = (const vec3_t&)(*this);
+        const vec3_t& r = (const vec3_t&)(rhs);
+        vec3_t v = l.cross(r);
+        line2d_t& answer = (line2d_t&)(v);
+        return answer;
+    }
 
 } // namespace cmbcv
--- cppsrc/include/geom2d.hpp
+++ cppsrc/include/geom2d.hpp
@@ -1,44 +1,141 @@
 #include <cassert>
 
-#include <boost/units/quantity.hpp>
-#include <boost/units/systems/si/length.hpp>
-#include <boost/units/io.hpp>
-
-#include "measure.hpp"
-
 namespace cmbcv {
 
-    typedef double Real;
-    using boost::units::si::length;
-    using boost::units::quantity;
-
-    class Point2D
+    struct vec3_t 
     {
-    public:
-        quantity<length> x;
-        quantity<length> y;
+        double x;
+        double y;
+        double z;
 
-        Point2D() {x = y = 0;}
+        vec3_t(double x, double y, double z) 
+            : x(x), y(y), z(z) {}
 
-        Point2D(const quantity<length>& x, const quantity<length>& y) 
+        double& operator[](int ix) {
+            assert(ix >= 0);
+            assert(ix <= 2);
+            return (&x)[ix];
+        }
+        const double& operator[](int ix) const {
+            assert(ix >= 0);
+            assert(ix <= 2);
+            return (&x)[ix];
+        }
+
+        // cross product
+        vec3_t cross(const vec3_t& rhs) const {
+            const vec3_t& lhs = *this;
+            return vec3_t(
+                lhs[1]*rhs[2] - lhs[2]*rhs[1],
+                lhs[2]*rhs[0] - lhs[0]*rhs[2],
+                lhs[0]*rhs[1] - lhs[1]*rhs[0]);
+        }
+    };
+
+    // forward declarations
+    struct point2d_t;
+    struct homogeneous_point2d_t;
+    struct line2d_t;
+
+    // point2d_t describes pixel coordinates in an image.
+    // point2d_t represents a column vector.
+    struct point2d_t
+    {
+        double x;
+        double y;
+
+        point2d_t() {x = y = 0;}
+
+        point2d_t(double x, double y) 
             : x(x), y(y) {}
 
-        Point2D(Real x, Real y, length u) 
-            : x(x*u), y(y*u) {}
-
-        quantity<length>& operator[](int ix) {
+        double& operator[](int ix) {
             assert(ix >= 0);
             assert(ix <= 1);
             return (&x)[ix];
         }
-        const quantity<length>& operator[](int ix) const {
+        const double& operator[](int ix) const {
             assert(ix >= 0);
             assert(ix <= 1);
             return (&x)[ix];
         }
 
-        Point2D operator+(const Point2D& rhs) const;
-        Point2D operator-(const Point2D& rhs) const;
+        point2d_t operator+(const point2d_t& rhs) const {
+            const point2d_t& lhs = *this;
+            return point2d_t(lhs.x + rhs.x, lhs.y + rhs.y);
+        }
+    };
+
+    struct homogeneous_point2d_t
+    {
+        double x, y, w; // w is inverse scale
+
+        homogeneous_point2d_t() 
+            : x(0.0), y(0.0), w(1.0) {}
+
+        homogeneous_point2d_t(double x, double y) 
+            : x(x), y(y), w(1.0) {}
+
+        homogeneous_point2d_t(double x, double y, double w) 
+            : x(x), y(y), w(w) {}
+
+        // point2d => homogeneous_point2d is cheap;
+        // the reverse is not.
+        homogeneous_point2d_t(const point2d_t& p) 
+            : x(p.x), y(p.y), w(1.0) {}
+
+        // TODO - is it possible to make this explicit?
+        // perhaps with an intermediate class with a type conversion...
+        operator point2d_t() const {return point2d_t(x/w, y/w);}
+
+        double& operator[](int ix) {
+            assert(ix >= 0);
+            assert(ix <= 2);
+            return (&x)[ix];
+        }
+        const double& operator[](int ix) const {
+            assert(ix >= 0);
+            assert(ix <= 2);
+            return (&x)[ix];
+        }
+
+        // TODO other arithmetic operators
+        point2d_t operator+(const homogeneous_point2d_t& rhs) const {
+            const homogeneous_point2d_t& lhs = *this;
+            return point2d_t(lhs) + point2d_t(rhs);
+        }
+
+        // line through two points
+        line2d_t line(const homogeneous_point2d_t& rhs) const;
+    };
+
+    struct line2d_t {
+        // equation of the line ax + bx + c = 0
+        double a;
+        double b;
+        double c;
+
+        line2d_t(double a, double b, double c) : a(a), b(b), c(c) {}
+
+        double& operator[](int ix) {
+            assert(ix >= 0);
+            assert(ix <= 2);
+            return (&a)[ix];
+        }
+        const double& operator[](int ix) const {
+            assert(ix >= 0);
+            assert(ix <= 2);
+            return (&a)[ix];
+        }
+
+        homogeneous_point2d_t intersection(const line2d_t& rhs) const {
+            // abuse vec3 cross product to get answer
+            const vec3_t& l = (const vec3_t&)(*this);
+            const vec3_t& r = (const vec3_t&)(rhs);
+            vec3_t v = l.cross(r);
+            homogeneous_point2d_t& answer = (homogeneous_point2d_t&)(v);
+            return answer;
+        }
     };
 
 } // namespace cmbcv

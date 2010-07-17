86455aef498c32e67db031ad8492b65665a9ec42
Christopher.Bruns <Christopher.Bruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-17 18:19:47 +0000

    Start move toward configurable setup.py file. Start move toward python testing.

---
+++ pysrc/tests/test_point2d.py
@@ -0,0 +1,15 @@
+import unittest
+import cmbcv
+
+class TestPoint2d(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_point2d(self):
+        self.assertEqual(self.seq, range(10))
+        self.assertTrue(element in self.seq)
+
+if __name__ == '__main__':
+    unittest.main()
+

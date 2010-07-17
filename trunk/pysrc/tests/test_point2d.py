bcdaf322bf91a416ea5a6a3500975b7e86a2ea98
ckbruns <ckbruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-17 21:41:08 +0000

    Experimented with rearranging test_point2d.py parameters.

--- trunk/pysrc/tests/test_point2d.py
+++ trunk/pysrc/tests/test_point2d.py
@@ -4,12 +4,12 @@ import cmbcv
 class TestPoint2d(unittest.TestCase):
 
     def setUp(self):
-        self.vec1 = cmbcv.point2d_t(1,2)
+        self.vec1 = cmbcv.point2d_t(5,2)
 
     def testPoint2d(self):
         v = self.vec1
         self.assertEqual(1+1, 2)
-        self.assertEqual(v.x, 1)
+        self.assertEqual(v.x, 5)
         self.assertEqual(v.y, 2)
 
 if __name__ == '__main__':

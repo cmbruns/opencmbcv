5164617214616cfe7914a2eaad75966c60302e4b
ckbruns <ckbruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-17 23:27:48 +0000

    Did testing of == and != for plain numbers.  Created TestPlan document for the tester.

--- trunk/pysrc/tests/test_point2d.py
+++ trunk/pysrc/tests/test_point2d.py
@@ -2,7 +2,6 @@ import unittest
 import cmbcv
 
 class TestPoint2d(unittest.TestCase):
-
     def setUp(self):
         self.vec1 = cmbcv.point2d_t(5,2)
 
@@ -11,6 +10,20 @@ class TestPoint2d(unittest.TestCase):
         self.assertEqual(1+1, 2)
         self.assertEqual(v.x, 5)
         self.assertEqual(v.y, 2)
+
+    def testNumberEquality(self):
+        self.assertEqual(5, 5)
+        #self.assertEqual(5, 4)
+        self.assertTrue(5 == 5)
+        #self.assertTrue(5 == 4)
+        self.assertFalse(5 == 4)
+        #self.assertFalse(5 == 5)
+        self.assertFalse(5 != 5)
+        #self.assertFalse(5 != 4)
+        self.assertTrue(5 != 4)
+        #self.assertTrue(5 != 5)
+        self.assertEqual([1, 2], [1, 2])
+        #self.assertEqual([1, 2], [1, 1])
 
 if __name__ == '__main__':
     unittest.main()

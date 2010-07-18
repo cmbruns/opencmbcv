e09b00f83ae4c46dd13290f03e4244adc425ee9d
ckbruns <ckbruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-18 00:05:51 +0000

    Created a point2d equality test that failed.

--- trunk/pysrc/tests/test_point2d.py
+++ trunk/pysrc/tests/test_point2d.py
@@ -13,25 +13,35 @@ class TestPoint2d(unittest.TestCase):
 
     def testNumberEquality(self):
         self.assertEqual(5, 5)
-        #self.assertEqual(5, 4)
+        # self.assertEqual(5, 4)
         self.assertTrue(5 == 5)
-        #self.assertTrue(5 == 4)
+        # self.assertTrue(5 == 4)
         self.assertFalse(5 == 4)
-        #self.assertFalse(5 == 5)
+        # self.assertFalse(5 == 5)
         self.assertFalse(5 != 5)
-        #self.assertFalse(5 != 4)
+        # self.assertFalse(5 != 4)
         self.assertTrue(5 != 4)
-        #self.assertTrue(5 != 5)
-        # Compare lists to lists
+        # self.assertTrue(5 != 5)
+        
+    def testListEquality(self):
         self.assertEqual([1, 2], [1, 2])
         #self.assertEqual([1, 2], [1, 1])
-        # Compare tuples to tuples
+        
+    def testTupleEquality(self):
         self.assertEqual((1, 3), (1, 3))
         self.assertEqual(tuple([1, 3]), (1, 3))
-        # It appears that tuples cannot be compared to lists with == nor !=
-        # self.assertEqual((1, 3), [1, 3])
-        self.assertTrue((1, 3) == [1, 3])
-
+        
+    def testPoint2dEquality(self):
+        v = self.vec1
+        self.assertEqual(v, v)
+        v2 = cmbcv.point2d_t(5,2)
+        self.assertEqual(v.x, v2.x)
+        self.assertEqual(v.y, v2.y)
+        self.assertEqual(v, v2)
+    
+    # It appears that tuples cannot be compared to lists with == nor !=
+        
+        
 if __name__ == '__main__':
     unittest.main()
 

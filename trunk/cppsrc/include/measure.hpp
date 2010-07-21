76a927eeb530b2bce82fa1843afa82ad8f552b33
Christopher.Bruns <Christopher.Bruns@1f01f6ea-e675-d132-ccb2-4cd361804335>
2010-07-21 22:46:27 +0000

    Changes to measure.hpp (which is not used)

--- trunk/cppsrc/include/measure.hpp
+++ trunk/cppsrc/include/measure.hpp
@@ -2,14 +2,20 @@
 // Pyplusplus/gccxml chokes on boost.units code
 // So create a crude facsimile here
 
-namespace cmbcv { namespace measure {
+namespace cmbcv {
+
+// "floats" in python are C++ doubles.  So use double.
+typedef double real_t;
 
 // Physical dimension class
 template<
-    int LENEXP, 
-    int ANGLEEXP,
-    int PIXELEXP>
-struct physical_dimension_t {};
+    int LENEXP, // length dimension exponent
+    int ANGLEEXP, // angle dimension exponent
+    int PIXELEXP> // pixel dimension exponent
+struct physical_dimension_t 
+{
+    // TODO - multplication, power, negation
+};
 
 typedef physical_dimension_t<1,0,0> length_t;
 typedef physical_dimension_t<2,0,0> Area;
@@ -17,11 +23,11 @@ typedef physical_dimension_t<3,0,0> Volume;
 typedef physical_dimension_t<0,1,0> Angle;
 typedef physical_dimension_t<0,0,1> Pixels;
 
-// Unit class
+// Unit class, templated by physical dimension
 template<class PDIM>
 struct unit_t 
 {
-    typedef PDIM physical_dimension_t;
+    typedef PDIM dimension_type;
 
     // Adding or subtracting compatible units yield first unit
     const unit_t<PDIM>& operator+(const unit_t<PDIM>& rhs) const {
@@ -35,13 +41,13 @@ struct unit_t
 static unit_t<length_t> meter;
 
 // Quantity class
-template<class UNIT, class VALTYPE=double>
+template<class UNIT, class VALTYPE=real_t>
 struct quantity_t 
 {
-    typedef VALTYPE value_t;
-    typedef UNIT unit_t;
+    typedef VALTYPE value_type;
+    typedef UNIT unit_type;
 
-    value_t value;
+    value_type value;
 };
 
-}} // namespace cmbcv
+} // namespace cmbcv
